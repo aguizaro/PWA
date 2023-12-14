06f14f47ca4776e6fc9d8dce09c29e6d7194aa1c
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-12-13 17:03:23 -0800

    added proj files

---
+++ README.md
@@ -0,0 +1,7 @@
+# StickerPAD
+
+Sketch pad program with emoji stickers built using Typescript and Vite.
+
+This was a project for CMPM:121 Game Development Patterns.
+
+[Live Demo](https://aguizaro.github.io/stickerPAD/)
---
+++ sw.js
@@ -0,0 +1,36 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
+
+// install event
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+      caches.open(staticCacheName).then((cache) => {
+        console.log('caching shell assets');
+        return cache.addAll(assets)
+          .catch(error => {
+            console.error('Cache.addAll failed:', error);
+          });
+      })
+    );
+  });
+  
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+});
