e1f9b5f94eb3731680a11e3768fe50e3a55f6aa0
Antonio Guizar Orozco <aguizaro@ucsc.edu>
2023-12-13 17:15:02 -0800

    change service worker cache

--- sw.js
+++ sw.js
@@ -1,27 +1,7 @@
-const staticCacheName = 'site-static';
-const assets = [
-  '/',
-  '/index.html',
-  '/js/app.js',
-  '/js/ui.js',
-  '/js/materialize.min.js',
-  '/css/styles.css',
-  '/css/materialize.min.css',
-  'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
-];
 
 // install event
 self.addEventListener('install', evt => {
-    evt.waitUntil(
-      caches.open(staticCacheName).then((cache) => {
-        console.log('caching shell assets');
-        return cache.addAll(assets)
-          .catch(error => {
-            console.error('Cache.addAll failed:', error);
-          });
-      })
-    );
+    //console.log('service worker installed');
   });
   
 
